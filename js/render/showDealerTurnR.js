706514cc84da148e139b4d34f9b55154fa07a8cd
chrisshepard <43590660+chrisshepard@users.noreply.github.com>
2018-12-18 01:29:20 -0500

    dealerturnshows

--- js/render/showDealerTurnR.js
+++ js/render/showDealerTurnR.js
@@ -1,3 +1,14 @@
 function showDealerTurnR(dealer) {
+  setTimeout(() => {
+    dealer.hands.forEach(function (card, cardPos) {
+      setTimeout(() => {
+        if (cardPos <= 1) {
 
+        } else {
+          var thisHand = document.getElementById("handd0");
+          thisHand.appendChild(cardComponent("d", cardPos, card.value, card.suit, cardPos));
+        };
+      }, 1000 * (cardPos - 1));
+    })
+  }, 1200);
 }
